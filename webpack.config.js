e407d68f685e65d715edfe594016e65637953258
Andre Meyer <andre.meyer93@gmail.com>
2020-07-09 16:28:50 +0200

    lint webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,56 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path')
-const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   module: {
     rules: [
       {
         test: /\.(jsx?)$/,
         exclude: /(node_modules)/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
       {
         test: /\.html$/,
-        use: ['html-loader'],
+        use: ["html-loader"],
       },
       {
         test: /\.json$/,
-        use: ['json-x-loader?exclude=bytecode+deployedBytecode+ast+legacyAST+sourceMap+deployedSourceMap+source+sourcePath+ast+legacyAST+compiler+schemaVersion+updatedAt+devdoc+userdoc']
-      }
-    ]
+        use: [
+          // allows artifact loading with a lower filesize by omitting all keys except abi, network and events
+          "json-x-loader?exclude=bytecode+deployedBytecode+ast+legacyAST+sourceMap+deployedSourceMap+source+sourcePath+ast+legacyAST+compiler+schemaVersion+updatedAt+devdoc+userdoc",
+        ],
+      },
+    ],
   },
   resolve: {
     modules: [
-      'src', // allows absolute imports like `import "components/App"`
-      'build',
-      'node_modules'
+      "src", // allows absolute imports like `import "components/App"`
+      "build",
+      "node_modules",
     ],
     alias: {
-      'react-dom': '@hot-loader/react-dom',
+      "react-dom": "@hot-loader/react-dom",
     },
   },
   devServer: {
     historyApiFallback: true,
     disableHostCheck: true,
-    contentBase: [
-      path.resolve(__dirname, 'assets')
-    ],
+    contentBase: [path.resolve(__dirname, "assets")],
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*",
     },
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'html', 'index.html')
+      template: path.resolve(__dirname, "src", "html", "index.html"),
     }),
     new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development',
-      NETWORK: 'local',
+      NODE_ENV: "development",
+      NETWORK: "local",
       INFURA_API_KEY: null,
-    })
-  ]
-}
+    }),
+  ],
+};
