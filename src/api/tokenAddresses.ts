25791c52d3b211721d25a1a87b0b1649d2a2e5cb
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-08-17 11:04:22 -0700

    Refactoring network definition in central place

--- src/api/tokenAddresses.ts
+++ src/api/tokenAddresses.ts
@@ -1,22 +1,17 @@
 // TODO: refactor and get these from the same file instead of repeating it everywhere
-export enum Network {
-  Mainnet = 1,
-  Ropsten = 3,
-  Rinkeby = 4,
-  Goerli = 5,
-  Kovan = 42,
-}
+
+import { Network } from "utils/constants";
 
 // TODO: dynamically load from a TCR
 const tokenAddresses: { [networkId: number]: string[] } = {
-  [Network.Mainnet]: [
+  [Network.mainnet]: [
     "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // weth
     "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // usdc
     "0x6B175474E89094C44Da98b954EedeAC495271d0F", // dai
     "0x6810e776880C02933D47DB1b9fc05908e5386b96", // gno
     "0x1A5F9352Af8aF974bFC03399e3767DF6370d82e4", // owl
   ],
-  [Network.Rinkeby]: [
+  [Network.rinkeby]: [
     "0xc778417E063141139Fce010982780140Aa0cD5Ab", // weth
     "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b", // usdc
     "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa", // dai
--- src/api/tx/index.js
+++ src/api/tx/index.js
@@ -1,16 +1,6 @@
-const NETWORK = process.env.NETWORK || "local";
+import { SAFE_ENDPOINT_URL, NETWORK } from "../../utils/constants";
 
-const RINKEBY = "rinkeby";
-const MAINNET = "mainnet";
-
-const NETWORK_API_URLS = {
-  [RINKEBY]: `https://safe-transaction.rinkeby.gnosis.io/`,
-  [MAINNET]: `https://safe-transaction.gnosis.io/`,
-};
-
-export const API_URL = NETWORK_API_URLS[NETWORK];
-
-if (!API_URL) {
+if (!SAFE_ENDPOINT_URL) {
   throw new Error(
     `Network ${NETWORK} has no API_URL configured. - Please define a different network using the env variable NETWORK or add the API_URL to src/api/tx/index.js`
   );
@@ -18,10 +8,10 @@ if (!API_URL) {
 
 export const getTransactions = async (safeAddress) => {
   const response = await fetch(
-    `${API_URL}/api/v1/safes/${safeAddress}/transactions`
+    `${SAFE_ENDPOINT_URL}/api/v1/safes/${safeAddress}/transactions`
   );
   const txs = await response.json();
-  console.log(txs)
+  console.log(txs);
 
   return txs.results;
 };
