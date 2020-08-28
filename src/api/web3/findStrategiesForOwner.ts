23223d499345eea8ac5ffd85d1b4d58e596f8aa5
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-08-28 09:56:07 -0700

    Filter self fleet deploy events Plus noise from auto-formating and a small clean up

--- src/api/web3/findStrategiesForOwner.ts
+++ src/api/web3/findStrategiesForOwner.ts
@@ -1,58 +1,32 @@
-import { Web3Context } from "types"
-import Strategy from 'logic/strategy';
+import { Web3Context } from "types";
+import Strategy from "logic/strategy";
 
-const findStrategiesForOwner = async (context : Web3Context) : Promise<Strategy[]> => {
+const findStrategiesForOwner = async (
+  context: Web3Context
+): Promise<Strategy[]> => {
   const {
     safeInfo: { safeAddress: owner },
     getDeployed,
-    getContract,
-    instance: web3
-  } = context
-  const fleetFactory = await getDeployed('FleetFactoryDeterministic');
-  const fleetDeployEvents = await fleetFactory.getPastEvents('FleetDeployed', { fromBlock: 0, toBlock: "latest" })
+  } = context;
+  const fleetFactory = await getDeployed("FleetFactoryDeterministic");
+  const fleetDeployEvents = await fleetFactory.getPastEvents("FleetDeployed", {
+    fromBlock: 0,
+    toBlock: "latest",
+    filter: { owner },
+  });
 
-  const strategies : Strategy[] = await Promise.all(
-    fleetDeployEvents.map(async (fleetDeployEvent) : Promise<Strategy> => {
-      const strategy = new Strategy(fleetDeployEvent);
+  const strategies: Strategy[] = await Promise.all(
+    fleetDeployEvents.map(
+      async (fleetDeployEvent): Promise<Strategy> => {
+        const strategy = new Strategy(fleetDeployEvent);
 
-      await strategy.fetchAllPossibleInfo(context);
-      return strategy;
-    })
-  )
+        await strategy.fetchAllPossibleInfo(context);
+        return strategy;
+      }
+    )
+  );
 
-  /*
-
-
-  console.log(strategyDeployedEvents)
-
-  let tokenAddressesFound = {}
-
-  const strategies = await Promise.all(
-    strategyDeployedEvents.map(async ({ transactionHash, blockNumber, blockHash, returnValues: { fleet } }) => {
-      const transaction = await web3.eth.getTransactionReceipt(transactionHash)
-      console.log(transaction)
-      const block = await web3.eth.getBlock(blockNumber);
-      const brackets = await Promise.all(
-        fleet.map(async (address) => {
-          const bracketContract = await getContract('GnosisSafe', address);
-          return bracketContract
-        })
-      )
-      
-      const exchangeEvents = await exchange.getPastEvents('allEvents', { fromBlock: blockNumber, toBlock: blockNumber })
-      console.log(exchangeEvents)
-
-      return {
-        blockHash,
-        brackets,
-        exchangeEvents,
-        block,
-      };
-    })
-  )
-  */
-
-  return strategies
-}
+  return strategies;
+};
 
 export default findStrategiesForOwner;
