58d2c7f508c1eb1d961c4a926cb5844d2fdac73e
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-01 14:36:58 -0700

    Added Link component

---
+++ src/components/basic/inputs/Link/Link.stories.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import { action } from "@storybook/addon-actions";
+
+import { Link, Props } from ".";
+
+export default {
+  component: Link,
+  title: "basic/input/Link",
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+};
+
+const onSubmit = (e: React.FormEvent) => e.preventDefault();
+
+export const linkData = {
+  type: "button",
+  color: "primary",
+  textSize: "lg",
+  children: "link",
+  href: "http://going.somewhere",
+} as Props;
+
+export const actionData = { onClick: action("onClick") };
+
+const Template = (args: Props): JSX.Element => {
+  return (
+    // <form noValidate autoComplete="off" onSubmit={onSubmit}>
+    <Link {...args} />
+    // </form>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = { ...linkData, ...actionData };
+
+export const TextIsANumber = Template.bind({});
+TextIsANumber.args = { ...Default.args, children: "56446.44" };
