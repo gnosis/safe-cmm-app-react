978995aeb834ac593cac39ab8084700fc9dbe60f
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-02 13:57:37 -0700

    Added MarketPrice component

---
+++ src/components/basic/display/MarketPrice/index.tsx
@@ -0,0 +1,48 @@
+import React, { useMemo } from "react";
+
+import { useTokenDetails } from "hooks/useTokenDetails";
+import { useGetPrice } from "hooks/useGetPrice";
+
+import { build1InchPriceUrl } from "utils/priceUrls";
+
+import { MarketPriceViewer } from "./viewer";
+import { useEffect } from "@storybook/addons";
+
+export interface Props {
+  tokenAAddress?: string;
+  tokenBAddress?: string;
+  setError?: (msg?: string) => void;
+}
+
+export const MarketPrice = (props: Props): JSX.Element => {
+  const { tokenAAddress, tokenBAddress, setError } = props;
+
+  const tokenA = useTokenDetails(tokenAAddress);
+  const tokenB = useTokenDetails(tokenBAddress);
+
+  // TODO: propagate error up to parent when dealing with validation
+  const { price, isLoading, error } = useGetPrice({
+    baseToken: tokenA,
+    quoteToken: tokenB,
+    source: "1inch",
+  });
+
+  useEffect(() => {
+    setError && setError(error);
+  }, [error]);
+
+  const priceUrl = useMemo((): string => build1InchPriceUrl(tokenA, tokenB), [
+    tokenA,
+    tokenB,
+  ]);
+
+  return (
+    <MarketPriceViewer
+      tokenA={tokenA}
+      tokenB={tokenB}
+      isPriceLoading={isLoading}
+      price={price ? price.toString() : ""}
+      priceUrl={priceUrl}
+    />
+  );
+};
