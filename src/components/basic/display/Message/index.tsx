b0f4c098c209b31664f580caeb758a2e740edbbe
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-04 13:11:24 -0700

    Added Message component

---
+++ src/components/basic/display/Message/index.tsx
@@ -0,0 +1,77 @@
+import React, { memo, useMemo } from "react";
+import styled from "styled-components";
+import { Icon, Text } from "@gnosis.pm/safe-react-components";
+
+import { theme } from "theme";
+
+export interface Props {
+  type: "error" | "warning";
+  label: string;
+}
+
+const Wrapper = styled.div<Props>`
+  display: flex;
+  flex-direction: column;
+
+  padding: 14px 15px 12px;
+
+  background-color: ${({ type }): string =>
+    theme.colors[`background${type[0].toUpperCase()}${type.slice(1)}`]};
+
+  border-radius: 6px;
+  border: 1px solid
+    ${({ type }): string =>
+      type === "error"
+        ? theme.colors.borderError
+        : theme.colors.backgroundWarning};
+
+  & > div {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    svg {
+      padding-right: 0.25em;
+    }
+  }
+
+  .msgBody {
+    padding-top: 12px;
+  }
+`;
+
+const component: React.FC<Props> = (props) => {
+  const { type, label, children } = props;
+
+  const msgBody = useMemo((): React.ReactNode => {
+    if (!children) {
+      return children;
+    } else if (typeof children === "string") {
+      return (
+        <Text size="sm" color="shadow" className="msgBody">
+          {children}
+        </Text>
+      );
+    } else {
+      return <div className="msgBody">{children}</div>;
+    }
+  }, [children]);
+
+  return (
+    <Wrapper {...props}>
+      <div>
+        <Icon
+          size="sm"
+          type={type === "warning" ? "alert" : "error"}
+          color={type}
+        />
+        <Text size="md" strong>
+          {label}
+        </Text>
+      </div>
+      {msgBody}
+    </Wrapper>
+  );
+};
+
+export const Message = memo(component);
