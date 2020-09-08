f95fc2a6e6b59b9fe275759d460d96a775423ce7
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-08 15:19:35 -0700

    Transformed GlobalStyle.js to .ts

--- src/types/global-extensions.ts
+++ src/types/global-extensions.ts
@@ -4,6 +4,12 @@ declare module "*.svg" {
   export default content;
 }
 
+declare module "*.woff2" {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const content: any;
+  export default content;
+}
+
 declare module "*.png" {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   const value: any;
