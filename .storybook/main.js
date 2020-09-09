d332d872d6b55b043980c937e30b5b0aa49851bc
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-09 14:16:43 -0700

    Work around dex-liquitidity import issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-const { EnvironmentPlugin } = require('webpack')
+const { EnvironmentPlugin } = require("webpack");
 
 const path = require("path");
 
@@ -11,7 +11,9 @@ const custom = require("../webpack.config.js");
 
 // the minimum plugins required
 // to inline CONFIG and other env vars
-const customPlugins = custom.plugins.filter(pl => pl instanceof EnvironmentPlugin)
+const customPlugins = custom.plugins.filter(
+  (pl) => pl instanceof EnvironmentPlugin
+);
 
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
@@ -22,18 +24,23 @@ module.exports = {
     "@storybook/addon-controls",
   ],
   webpackFinal: (config) => {
-
     // I know, I know, using obj destruction assignment (config = {...config, prop: overwrite}) looks better, etc
     // but after a few hours fighting with the build I figured it out it wasn't working. And, I found out that
     // this way works, so, there you have it. Happy to change back if anyone can show how to make it work.
 
     // important for absolute path resolution
-    Object.keys(custom.resolve.modules).forEach(key => config.resolve.modules[key] = custom.resolve.modules[key])
+    Object.keys(custom.resolve.modules).forEach(
+      (key) => (config.resolve.modules[key] = custom.resolve.modules[key])
+    );
+
     // enable plugins override sparingly
     // only if storybook doesn't work as is
     // pulling in every plugin currently breaks production build
-    config.plugins = config.plugins.concat(customPlugins)
+    config.plugins = config.plugins.concat(customPlugins);
 
-    return config
-  }
-}
+    // Get the `jsx?` rule to skip `@gnosis.pm/dex-` files, currently causing issues
+    config.module.rules.push(custom.module.rules[0]);
+
+    return config;
+  },
+};
