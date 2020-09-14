ee871af53023a43e0c5feb4839e3953c8aab32f1
Velenir <velenir@gmail.com>
2020-09-14 17:04:38 +0300

    simplify mock/useGetPrice

--- src/mock/useGetPrice.ts
+++ src/mock/useGetPrice.ts
@@ -1,11 +1,5 @@
-import Decimal from "decimal.js";
-
 import { useGetPrice as useGetPriceHook } from "hooks/useGetPrice";
-import { Parameters as StoryParameters, StoryContext } from "@storybook/react";
-
-let price: null | Decimal = null;
-let isLoading = false;
-let error = "";
+import { createMockHook } from "./mockHookContext";
 
 /**
  * Mock version of `useGetPrice` hook.
@@ -18,38 +12,15 @@ let error = "";
  *    isLoading: true,
  *    error: 'Something went wrong'
  *  }
+ * or
+ *  useGetPrice: (...argsPassedToHook) => ({
+ *    price: new Decimal('13'),
+ *    isLoading: true,
+ *    error: 'Something went wrong'
+ *  })
  * }
  */
-export function useGetPrice(
-  ...[params]: Parameters<typeof useGetPriceHook>
-): ReturnType<typeof useGetPriceHook> {
-  return {
-    price: !params.baseToken || !params.quoteToken ? null : price,
-    isLoading,
-    error,
-  };
-}
-
-interface UseGetPriceStoryParameters {
-  price?: Decimal;
-  isLoading?: boolean;
-  error?: string;
-}
-
-interface ExtendedContext extends StoryContext {
-  parameters: StoryParameters & { useGetPrice?: UseGetPriceStoryParameters };
-}
-
-/**
- * Storybook decorator
- */
-export function decorator(
-  story: any,
-  { parameters: { useGetPrice } }: ExtendedContext
-): any {
-  price = useGetPrice?.price || null;
-  isLoading = useGetPrice?.isLoading || false;
-  error = useGetPrice?.error || "";
-
-  return story();
-}
+export const useGetPrice = createMockHook<typeof useGetPriceHook>(
+  "useGetPrice",
+  { price: null, isLoading: false, error: "" }
+);
