7688ecad69f72757696e31abfe42b39aed14edc7
Velenir <velenir@gmail.com>
2020-09-14 17:02:47 +0300

    setup generic hook mock mechanism

---
+++ src/mock/mockHookContext.tsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react'
+import { BaseDecorators, Parameters } from '@storybook/addons'
+
+const MockHookContext = React.createContext<Parameters>({})
+
+/**
+ * create a mock hook with
+ * export const useYourOfHook = createMockHook('useYourOfHook)
+ * 
+ * in *.stories.tsx
+ * Story.parameter = {
+ *  useYourHook: {whatever hook should return}
+ * }
+ * 
+ * or
+ * Story.parameter = {
+ *  useYourHook: (...whatHookExpectsAsInput) => ({whatever hook should return})
+ * }
+ */
+export const createMockHook = <T extends (...args: any) => any>(name: string, defaultValue?: ReturnType<T>): T => {
+  return ((...args: any[]) => {
+    const ctx = useContext(MockHookContext)
+    console.log('ctx', JSON.stringify(ctx, null, 2));
+    const mockedHook = ctx[name]
+    // if parameters = {useYourHook: Function}, then execute with args
+    if (typeof mockedHook === 'function') {
+      const res = mockedHook(...args)
+      return res
+    }
+    // if not Function, return as is or defaultValue if Nullable
+    return mockedHook ?? defaultValue
+  }) as T
+}
+
+export const mockHookDecorator: BaseDecorators<JSX.Element>[0] = (Story, { parameters }) => {
+  console.log('parameters', parameters);
+  return (
+    <MockHookContext.Provider value={parameters}>
+      {Story()}
+    </MockHookContext.Provider>
+  )
+}
