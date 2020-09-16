58a0cb9455b1c742c20b962600b4e776b928e499
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-16 15:04:55 -0700

    New hook useDeployStrategy encapsulating deployStrategy logic

---
+++ src/hooks/useDeployStrategy.ts
@@ -0,0 +1,71 @@
+import { useContext } from "react";
+import BN from "bn.js";
+
+import { parseAmount } from "@gnosis.pm/dex-js";
+
+import { Web3Context } from "components/Web3Provider";
+import deployStrategy from "api/deployStrategy";
+
+import { useTokenDetails } from "hooks/useTokenDetails";
+
+export interface Params {
+  lowestPrice: string;
+  highestPrice: string;
+  baseTokenAmount: string;
+  quoteTokenAmount: string;
+  totalBrackets: string;
+  baseTokenAddress: string;
+  quoteTokenAddress: string;
+  startPrice: string;
+}
+
+export type Return = null | (() => Promise<void>);
+
+export function useDeployStrategy(params: Params): Return {
+  const {
+    lowestPrice,
+    highestPrice,
+    baseTokenAmount,
+    quoteTokenAmount,
+    totalBrackets,
+    baseTokenAddress,
+    quoteTokenAddress,
+    startPrice,
+  } = params;
+
+  const web3Context = useContext(Web3Context);
+  const { tokenDetails: baseTokenDetails } = useTokenDetails(baseTokenAddress);
+  const { tokenDetails: quoteTokenDetails } = useTokenDetails(
+    quoteTokenAddress
+  );
+
+  // simple validation
+  if (
+    !lowestPrice ||
+    !highestPrice ||
+    !startPrice ||
+    !baseTokenAmount ||
+    !quoteTokenAmount ||
+    !totalBrackets ||
+    !baseTokenAddress ||
+    !quoteTokenAddress ||
+    !baseTokenDetails ||
+    !quoteTokenDetails
+  ) {
+    return null;
+  }
+
+  return async (): Promise<void> => {
+    await deployStrategy(
+      web3Context,
+      +totalBrackets,
+      baseTokenAddress,
+      quoteTokenAddress,
+      parseAmount(lowestPrice, baseTokenDetails.decimals),
+      parseAmount(highestPrice, baseTokenDetails.decimals),
+      parseAmount(baseTokenAmount, baseTokenDetails.decimals),
+      parseAmount(quoteTokenAmount, quoteTokenDetails.decimals),
+      new BN(startPrice.toString())
+    );
+  };
+}
---
+++ src/mock/useDeployStrategy.ts
@@ -0,0 +1,7 @@
+import { useDeployStrategy as hook } from "hooks/useDeployStrategy";
+import { createMockHook } from "./mockHookContext";
+
+export const useDeployStrategy = createMockHook<typeof hook>(
+  "useDeployStrategy",
+  null
+);
