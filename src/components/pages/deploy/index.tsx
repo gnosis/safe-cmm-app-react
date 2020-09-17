359a939b8259a25a7d9f84fb6bd2d70d178b9c84
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-17 15:52:11 -0700

    Refactored DeployPage container to use Recoil

--- src/components/pages/deploy/index.tsx
+++ src/components/pages/deploy/index.tsx
@@ -1,6 +1,29 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useEffect, useMemo } from "react";
+import {
+  RecoilState,
+  useRecoilCallback,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 
 import { useDeployStrategy } from "hooks/useDeployStrategy";
+
+import {
+  baseTokenAddressAtom,
+  quoteTokenAddressAtom,
+  lowestPriceAtom,
+  startPriceAtom,
+  highestPriceAtom,
+  baseTokenAmountAtom,
+  quoteTokenAmountAtom,
+  totalBracketsAtom,
+  baseTokenBracketsAtom,
+  quoteTokenBracketsAtom,
+  errorAtom,
+  isSubmittingAtom,
+  isValidAtom,
+} from "./atoms";
 
 import { DeployPageViewer, Props } from "./viewer";
 
@@ -12,23 +35,19 @@ const onChangeHandlerFactory = (
 
 export function DeployPage(): JSX.Element {
   // input states
-  const [baseTokenAddress, setBaseTokenAddress] = useState("");
-  const [quoteTokenAddress, setQuoteTokenAddress] = useState("");
-  const [lowestPrice, setLowestPrice] = useState("");
-  const [startPrice, setStartPrice] = useState("");
-  const [highestPrice, setHighestPrice] = useState("");
-  const [baseTokenAmount, setBaseTokenAmount] = useState("");
-  const [quoteTokenAmount, setQuoteTokenAmount] = useState("");
-  const [totalBrackets, setTotalBrackets] = useState("");
-  // display states
-  const [baseTokenBrackets, setBaseTokenBrackets] = useState(0);
-  const [quoteTokenBrackets, setQuoteTokenBrackets] = useState(0);
-  const [totalInvestment, setTotalInvestment] = useState("");
-  // error handling
-  const [error, setError] = useState<{ label: string; body: string } | null>(
-    null
+  const baseTokenAddress = useRecoilValue(baseTokenAddressAtom);
+  const quoteTokenAddress = useRecoilValue(quoteTokenAddressAtom);
+  const [lowestPrice, setLowestPrice] = useRecoilState(lowestPriceAtom);
+  const [startPrice, setStartPrice] = useRecoilState(startPriceAtom);
+  const [highestPrice, setHighestPrice] = useRecoilState(highestPriceAtom);
+  const [baseTokenAmount, setBaseTokenAmount] = useRecoilState(
+    baseTokenAmountAtom
   );
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [quoteTokenAmount, setQuoteTokenAmount] = useRecoilState(
+    quoteTokenAmountAtom
+  );
+  const totalBrackets = useRecoilValue(totalBracketsAtom);
+  const setIsValid = useSetRecoilState(isValidAtom);
 
   const deployStrategy = useDeployStrategy({
     lowestPrice,
@@ -41,91 +60,94 @@ export function DeployPage(): JSX.Element {
     startPrice,
   });
 
-  const onSubmit = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+  const onSubmit = useRecoilCallback(
+    ({ set }) => async (
+      event: React.FormEvent<HTMLFormElement>
+    ): Promise<void> => {
       // TODO: move on to another page/show success message
       event.preventDefault();
 
       if (deployStrategy) {
-        setIsSubmitting(true);
-        setError(null);
+        set(isSubmittingAtom, true);
+        set(errorAtom, null);
         try {
           await deployStrategy();
         } catch (e) {
           console.error(`Failed to deploy strategy:`, e);
-          setError({ label: "Failed to deploy strategy", body: e.message });
+          set(errorAtom, {
+            label: "Failed to deploy strategy",
+            body: e.message,
+          });
         }
-        setIsSubmitting(false);
+        set(isSubmittingAtom, false);
       }
     },
     [deployStrategy]
   );
 
-  const onTotalBracketsChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>): void => {
-      setTotalBrackets(event.target.value);
+  const onTotalBracketsChange = useRecoilCallback(
+    ({ set }) => (event: React.ChangeEvent<HTMLInputElement>): void => {
+      set(totalBracketsAtom, event.target.value);
 
       const brackets = +event.target.value;
 
       // TODO: find out how to properly split brackets when uneven
       if (brackets >= 1) {
         if (brackets % 2 === 0) {
-          setBaseTokenBrackets(brackets / 2);
-          setQuoteTokenBrackets(brackets / 2);
+          set(baseTokenBracketsAtom, brackets / 2);
+          set(quoteTokenBracketsAtom, brackets / 2);
         } else {
-          setBaseTokenBrackets(Math.ceil(brackets / 2));
-          setQuoteTokenBrackets(Math.floor(brackets / 2));
+          set(baseTokenBracketsAtom, Math.ceil(brackets / 2));
+          set(quoteTokenBracketsAtom, Math.floor(brackets / 2));
         }
       } else {
-        setBaseTokenBrackets(0);
-        setQuoteTokenBrackets(0);
+        set(baseTokenBracketsAtom, 0);
+        set(quoteTokenBracketsAtom, 0);
       }
     },
     []
   );
 
-  const swapTokens = useCallback(() => {
-    setBaseTokenAddress(quoteTokenAddress);
-    setQuoteTokenAddress(baseTokenAddress);
-  }, [baseTokenAddress, quoteTokenAddress]);
+  const swapTokens = useRecoilCallback(({ snapshot, set }) => async (): Promise<
+    void
+  > => {
+    const [currBase, currQuote] = await Promise.all([
+      snapshot.getPromise(baseTokenAddressAtom),
+      snapshot.getPromise(quoteTokenAddressAtom),
+    ]);
+    set(quoteTokenAddressAtom, currBase);
+    set(baseTokenAddressAtom, currQuote);
+  });
 
-  const onSelectTokenFactory = useCallback(
-    (
-      setter: React.Dispatch<React.SetStateAction<string>>,
-      oppositeValue: string
-    ) => (address: string): void => {
+  const onSelectTokenFactory = useRecoilCallback(
+    ({ snapshot, set }) => (
+      currentSelectAtom: RecoilState<string>,
+      oppositeSelectAtom: RecoilState<string>
+    ) => async (address: string): Promise<void> => {
+      const oppositeValue = await snapshot.getPromise(oppositeSelectAtom);
+
       if (address === oppositeValue) {
         swapTokens();
       } else {
-        setter(address);
+        set(currentSelectAtom, address);
       }
-    },
-    [swapTokens]
+    }
   );
 
-  const viewerProps: Props = useMemo(() => {
-    return {
-      // inputs
-      baseTokenAddress,
-      quoteTokenAddress,
-      lowestPrice,
-      startPrice,
-      highestPrice,
-      baseTokenAmount,
-      quoteTokenAmount,
-      totalBrackets,
-      // display
-      baseTokenBrackets,
-      quoteTokenBrackets,
-      // callbacks
+  useEffect(() => {
+    setIsValid(!!deployStrategy);
+  }, [deployStrategy]);
+
+  const viewerProps: Props = useMemo(
+    () => ({
       swapTokens,
       onBaseTokenSelect: onSelectTokenFactory(
-        setBaseTokenAddress,
-        quoteTokenAddress
+        baseTokenAddressAtom,
+        quoteTokenAddressAtom
       ),
       onQuoteTokenSelect: onSelectTokenFactory(
-        setQuoteTokenAddress,
-        baseTokenAddress
+        quoteTokenAddressAtom,
+        baseTokenAddressAtom
       ),
       onLowestPriceChange: onChangeHandlerFactory(setLowestPrice),
       onStartPriceChange: onChangeHandlerFactory(setStartPrice),
@@ -134,34 +156,9 @@ export function DeployPage(): JSX.Element {
       onQuoteTokenAmountChange: onChangeHandlerFactory(setQuoteTokenAmount),
       onTotalBracketsChange,
       onSubmit: deployStrategy && onSubmit,
-      isSubmitting,
-      isValid: !!deployStrategy,
-      messages: error && [
-        {
-          type: "error",
-          label: error.label,
-          children: error.body,
-        },
-      ],
-    };
-  }, [
-    baseTokenAddress,
-    quoteTokenAddress,
-    lowestPrice,
-    startPrice,
-    highestPrice,
-    baseTokenAmount,
-    quoteTokenAmount,
-    totalBrackets,
-    baseTokenBrackets,
-    quoteTokenBrackets,
-    swapTokens,
-    onSelectTokenFactory,
-    deployStrategy,
-    onSubmit,
-    isSubmitting,
-    error,
-  ]);
+    }),
+    [deployStrategy, onSubmit]
+  );
 
   return <DeployPageViewer {...viewerProps} />;
 }
