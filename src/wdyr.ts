33cbf0cd18852ad421478c14aa6d973bfb6eb77a
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-22 08:07:13 -0700

    Disable WhyDidYouRender. Turn it on only when needed.

--- src/wdyr.ts
+++ src/wdyr.ts
@@ -1,9 +1,10 @@
-import React from "react";
+// import React from "react";
 
-if (process.env.NODE_ENV === "development") {
-  const whyDidYouRender = require("@welldone-software/why-did-you-render");
-  // console.log(`WHY DID YOU RENDER???`, whyDidYouRender);
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  });
-}
+// if (process.env.NODE_ENV === "development") {
+//   const whyDidYouRender = require("@welldone-software/why-did-you-render");
+//   // console.log(`WHY DID YOU RENDER???`, whyDidYouRender);
+//   whyDidYouRender(React, {
+//     trackAllPureComponents: true,
+//   });
+// }
+// TODO: uncomment when testing. Otherwise it's too much noise in the console.
