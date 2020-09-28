7dfb58072463308f4a2b3dca11762545fe7c3fa0
Andre Meyer <andre.meyer93@gmail.com>
2020-09-28 15:04:58 +0200

    fixes for withdraw, implementing script changes

--- src/api/eth/orderAndFund.js
+++ src/api/eth/orderAndFund.js
@@ -1,4 +1,4 @@
-import runInitializerIfNotRan from "api/utils/tradingHelperInit";
+import { importTradingStrategyHelpers } from "api/utils/dexImports";
 import Decimal from "decimal.js";
 
 const orderAndFund = async (
@@ -32,7 +32,7 @@ const orderAndFund = async (
   const {
     transactionsForTransferApproveDepositFromOrders,
     transactionsForOrders,
-  } = runInitializerIfNotRan(context);
+  } = importTradingStrategyHelpers(context);
 
   const batchExchangeContract = await context.getDeployed("BatchExchange");
   const [tokenBaseId, tokenQuoteId] = await Promise.all([
