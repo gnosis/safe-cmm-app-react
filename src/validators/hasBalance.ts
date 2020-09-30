cec3f3843f791f0a9ea38e3d92a7b1bcd57e0114
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-09-30 13:43:21 -0700

    New validator to check user balance

---
+++ src/validators/hasBalance.ts
@@ -0,0 +1,27 @@
+import { parseAmount } from "@gnosis.pm/dex-js";
+
+import { TokenBalance } from "types";
+
+import { Validator } from "./misc";
+
+/**
+ * Validator factory that takes in `getErc20Details` function from Web3Context
+ * to check whether given value is greater than balance for given token address
+ *
+ * @param getErc20Details Async function that takes in a tokenAddress and returns TokenBalance
+ */
+export const hasBalanceFactory = (
+  getErc20Details: (address: string) => Promise<TokenBalance>
+) => (tokenAddress: string): Validator => (fieldName) => async (value) => {
+  // Don't validate unless we have all the required input
+  if (!tokenAddress || !value) {
+    return undefined;
+  }
+  const details = await getErc20Details(tokenAddress);
+  const bnAmount = parseAmount(value, details.decimals);
+
+  if (bnAmount.gt(details.balance)) {
+    return { label: `Insufficient balance for ${details.symbol} token` };
+  }
+  return undefined;
+};
