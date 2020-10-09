6d04908c0978ccf00c36470ee97510930d1f373b
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-10-09 15:15:50 -0700

    More WIP, now adding the needle and associated upper/lower text

--- src/components/basic/display/BracketsView/BracketsView.stories.tsx
+++ src/components/basic/display/BracketsView/BracketsView.stories.tsx
@@ -9,13 +9,13 @@ export default {
   decorators: [(Story) => <div style={{ width: "400px" }}>{Story()}</div>],
 } as Meta;
 
-const minimumData: Props = { type: "deploy" };
+const requiredData: Props = { type: "deploy" };
 
 const Template: Story<Props> = (props) => <BracketsViewView {...props} />;
 
 export const DeployDefault = Template.bind({});
 DeployDefault.args = {
-  ...minimumData,
+  ...requiredData,
   totalBrackets: 5,
   leftBrackets: 3,
   rightBrackets: 2,
@@ -25,23 +25,76 @@ DeployDefault.args = {
 
   lowestPrice: "0",
   highestPrice: "0",
+  startPrice: "5.8",
 };
 
 export const DeployMinimal = Template.bind({});
-DeployMinimal.args = { ...minimumData };
+DeployMinimal.args = { ...requiredData };
 
-export const DeployAllLeft = Template.bind({});
-DeployAllLeft.args = {
+export const DeployAllBracketsLeft = Template.bind({});
+DeployAllBracketsLeft.args = {
   ...DeployDefault.args,
   totalBrackets: 3,
   leftBrackets: 3,
   rightBrackets: 0,
 };
 
-export const DeployAllRight = Template.bind({});
-DeployAllRight.args = {
+export const DeployAllBracketsRight = Template.bind({});
+DeployAllBracketsRight.args = {
   ...DeployDefault.args,
   totalBrackets: 3,
   leftBrackets: 0,
   rightBrackets: 3,
+};
+
+export const DeployStartPriceCloseToLowestPrice = Template.bind({});
+DeployStartPriceCloseToLowestPrice.args = {
+  ...DeployDefault.args,
+  lowestPrice: "1",
+  highestPrice: "5",
+  startPrice: "1.1",
+};
+
+export const DeployStartPriceAt25Percent = Template.bind({});
+DeployStartPriceAt25Percent.args = {
+  ...DeployDefault.args,
+  lowestPrice: "1",
+  highestPrice: "5",
+  startPrice: "2",
+};
+
+export const DeployStartPriceAt75Percent = Template.bind({});
+DeployStartPriceAt75Percent.args = {
+  ...DeployDefault.args,
+  lowestPrice: "1",
+  highestPrice: "5",
+  startPrice: "4",
+};
+
+export const DeployStartPriceCloseToHighestPrice = Template.bind({});
+DeployStartPriceCloseToHighestPrice.args = {
+  ...DeployDefault.args,
+  lowestPrice: "1",
+  highestPrice: "5",
+  startPrice: "4.9",
+};
+
+export const DeployStartPriceSmallerThanLowestPrice = Template.bind({});
+DeployStartPriceSmallerThanLowestPrice.args = {
+  ...DeployDefault.args,
+  lowestPrice: "1",
+  highestPrice: "5",
+  startPrice: "0.1",
+  leftBrackets: 0,
+  rightBrackets: 5,
+};
+
+export const DeployStartPriceGreaterThanHighestPrice = Template.bind({});
+DeployStartPriceGreaterThanHighestPrice.args = {
+  ...DeployDefault.args,
+  lowestPrice: "1",
+  highestPrice: "5",
+  startPrice: "5.5",
+  leftBrackets: 5,
+  rightBrackets: 0,
 };
--- src/components/basic/display/BracketsView/viewer.tsx
+++ src/components/basic/display/BracketsView/viewer.tsx
@@ -8,6 +8,7 @@ type PageType = "deploy" | "strategy";
 
 type WrapperProps = {
   hasLeft: boolean;
+  needlePosition?: number;
 };
 
 const LEFT_BACKGROUND_COLOR = "rgba(0, 156, 180, 0.1)";
@@ -16,7 +17,7 @@ const LEFT_BORDER_LIGHT = "2px solid rgba(0, 156, 180, 0.2)";
 const LEFT_BORDER_THICK = "2px solid #009cb4";
 const RIGHT_BORDER_LIGHT = "2px solid rgba(128, 94, 255, 0.2)";
 const RIGHT_BORDER_THICK = "2px solid #805eff";
-const MIDDLE_BORDER = "";
+const NEEDLE_BORDER = "1px dashed #008C73";
 
 const Wrapper = styled.div<WrapperProps>`
   height: 90px;
@@ -50,11 +51,59 @@ const Wrapper = styled.div<WrapperProps>`
     }
   }
 
-  /* TODO: Refactor into own component */
-  .container {
+  .bar {
     width: inherit;
     height: 46px;
+    position: relative;
+  }
 
+  /* TODO: refactor into own component */
+  ${({ needlePosition = 50 }) => `
+  .needle {
+    height: inherit;
+    width: 0;
+
+    border: ${NEEDLE_BORDER};
+    border-top: 0;
+    border-bottom: 0;
+
+    position: absolute;
+    left: ${needlePosition}%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: ${
+      needlePosition < 20
+        ? "flex-start"
+        : needlePosition > 80
+        ? "flex-end"
+        : "center"
+    };
+
+    .label {
+      text-transform: uppercase;
+
+      position: absolute;
+      width: max-content;
+      top: -1.6em;
+      ${needlePosition < 20 ? `left: 30px;` : ""}
+      ${needlePosition > 80 ? `right: 30px;` : ""}
+    }
+
+    .price {
+      position: absolute;
+      width: max-content;
+      bottom: -20px;
+      ${needlePosition < 20 ? `left: 30px;` : ""}
+      ${needlePosition > 80 ? `right: 30px;` : ""}
+
+    }
+  }
+  `}
+
+  /* TODO: Refactor into own component */
+  .container {
+    height: inherit;
     display: flex;
 
     & > * {
@@ -114,19 +163,85 @@ export type Props = {
 type PriceDisplayProps = {
   price?: string;
   token?: string;
+  color?: "primary" | "text";
   className?: string;
+  adornment?: "left" | "right";
 };
 
+const PriceDisplayWrapper = styled.div`
+  display: flex;
+
+  & > *:not(:last-child) {
+    margin-right: 0.25em;
+  }
+`;
+
 function PriceDisplay(props: PriceDisplayProps): JSX.Element {
-  const { price, token, className } = props;
+  const { price, token, className, color = "text", adornment } = props;
   return (
     // TODO: clear up inline styles
-    <div className={className} style={{ display: "flex" }}>
-      <Text size="sm" as="span" style={{ paddingRight: "0.25em" }}>
+    <PriceDisplayWrapper className={className}>
+      {adornment === "left" && (
+        <Text size="sm" as="span" color={color}>
+          &lt;
+        </Text>
+      )}
+      <Text size="sm" as="span" color={color}>
         {price}
       </Text>
-      <TokenDisplay token={token} size="sm" />
-    </div>
+      <TokenDisplay token={token} size="sm" color={color} />
+      {!!adornment && (
+        <Text size="sm" color="rinkeby">
+          (out of range)
+        </Text>
+      )}
+      {adornment === "right" && (
+        <Text size="sm" as="span" color={color}>
+          &gt;
+        </Text>
+      )}
+    </PriceDisplayWrapper>
+  );
+}
+
+/**
+ * Calculates needle position as a percentage.
+ * Percentage can be negative or greater than 100.
+ * Returns undefined when input is invalid
+ *
+ * @param startPrice Start price string
+ * @param lowestPrice Lowest Price string
+ * @param highestPrice Highest Price string
+ */
+function calculateNeedlePosition(
+  startPrice?: string,
+  lowestPrice?: string,
+  highestPrice?: string
+): number | undefined {
+  const sp = Number(startPrice);
+  const lp = Number(lowestPrice);
+  const hp = Number(highestPrice);
+
+  if (isNaN(sp) || isNaN(lp) || isNaN(hp) || lp >= hp) {
+    return undefined;
+  }
+
+  return (100 * (sp - lp)) / (hp - lp);
+}
+
+function NeedleLabel({
+  label,
+  adornment,
+  className,
+}: {
+  label: string;
+  adornment?: "left" | "right";
+  className?: string;
+}): JSX.Element {
+  return (
+    <Text as="span" size="sm" color="primary" strong className={className}>
+      {adornment === "left" && "<"} {label} {adornment === "right" && ">"}
+    </Text>
   );
 }
 
@@ -141,45 +256,104 @@ export const BracketsViewView = memo(function BracketsViewView(
     baseTokenAddress,
     quoteTokenAddress,
 
+    startPrice,
     lowestPrice,
     highestPrice,
+
+    type,
   } = props;
 
+  const needlePosition = calculateNeedlePosition(
+    startPrice,
+    lowestPrice,
+    highestPrice
+  );
+  const needleBelow0 = needlePosition && needlePosition < 0;
+  const needleAbove100 = !needleBelow0 && needlePosition > 100;
+
+  const labelText = type === "deploy" ? "START PRICE" : "MARKET PRICE";
+
   return (
-    <Wrapper hasLeft={!!leftBrackets}>
+    <Wrapper hasLeft={!!leftBrackets} needlePosition={needlePosition}>
       <div className="header">
-        {baseTokenAddress && (
-          <TokenDisplay token={baseTokenAddress} size="sm" />
+        {needleBelow0 ? (
+          <NeedleLabel label={labelText} adornment="left" />
+        ) : (
+          baseTokenAddress && (
+            <TokenDisplay token={baseTokenAddress} size="sm" />
+          )
         )}
-        {quoteTokenAddress && (
-          <TokenDisplay
-            token={quoteTokenAddress}
-            size="sm"
+        {needleAbove100 ? (
+          <NeedleLabel
+            label={labelText}
+            adornment="right"
             className="justifyRight"
           />
+        ) : (
+          quoteTokenAddress && (
+            <TokenDisplay
+              token={quoteTokenAddress}
+              size="sm"
+              className="justifyRight"
+            />
+          )
         )}
       </div>
-      <div className="container">
-        {range(
-          !leftBrackets && !rightBrackets ? totalBrackets : leftBrackets
-        ).map((id) => (
-          <div className="left" key={id} />
-        ))}
-        {range(rightBrackets).map((id) => (
-          <div className="right" key={id + leftBrackets} />
-        ))}
+      <div className="bar">
+        {!needleBelow0 && !needleAbove100 && (
+          <div className="needle">
+            <NeedleLabel label={labelText} className="label" />
+            {startPrice && (
+              <PriceDisplay
+                price={startPrice}
+                token={baseTokenAddress}
+                color="primary"
+                className="price"
+              />
+            )}
+          </div>
+        )}
+        <div className="container">
+          {range(
+            !leftBrackets && !rightBrackets ? totalBrackets : leftBrackets
+          ).map((id) => (
+            <div className="left" key={id} />
+          ))}
+          {range(rightBrackets).map((id) => (
+            <div className="right" key={id + leftBrackets} />
+          ))}
+        </div>
       </div>
       <div className="footer">
-        {lowestPrice && (
-          <PriceDisplay price={lowestPrice} token={baseTokenAddress} />
-        )}
-        {highestPrice && (
-          <PriceDisplay
-            price={highestPrice}
-            token={baseTokenAddress}
-            className="justifyRight"
-          />
-        )}
+        {needleBelow0
+          ? startPrice && (
+              <PriceDisplay
+                price={startPrice}
+                token={baseTokenAddress}
+                adornment="left"
+                color="primary"
+              />
+            )
+          : lowestPrice && (
+              <PriceDisplay price={lowestPrice} token={baseTokenAddress} />
+            )}
+        {needleAbove100
+          ? startPrice && (
+              <PriceDisplay
+                price={startPrice}
+                token={baseTokenAddress}
+                adornment="right"
+                color="primary"
+                className="justifyRight"
+              />
+            )
+          : highestPrice && (
+              <PriceDisplay
+                price={highestPrice}
+                token={baseTokenAddress}
+                className="justifyRight"
+              />
+            )}
       </div>
     </Wrapper>
   );
