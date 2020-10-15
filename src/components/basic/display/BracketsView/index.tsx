bd934fcc6bbaa4447de8dd10008bff5012e2f3b9
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-10-15 15:06:26 -0700

    Loading market price and calculating brackets for strategy page

--- src/components/basic/display/BracketsView/index.tsx
+++ src/components/basic/display/BracketsView/index.tsx
@@ -1,13 +1,55 @@
 import React, { memo } from "react";
 
+import { useGetPrice } from "hooks/useGetPrice";
+import { useTokenDetails } from "hooks/useTokenDetails";
+
+import { calculateBrackets } from "utils/calculateBrackets";
+
 import { BracketsViewView, Props as ViewerProps } from "./viewer";
 
-export type Props = Omit<ViewerProps, "lowerThreshold" | "upperThreshold">;
+export type Props = ViewerProps;
 
 export const BracketsViewer = memo(function BracketsViewer(
   props: Props
 ): JSX.Element {
-  // TODO: add logic for fetching market price when type === 'strategy'
+  const {
+    baseTokenAddress,
+    quoteTokenAddress,
+    type,
+    startPrice,
+    lowestPrice,
+    highestPrice,
+    totalBrackets,
+    leftBrackets,
+    rightBrackets,
+  } = props;
 
-  return <BracketsViewView {...props} />;
+  const isStrategy = type === "strategy";
+
+  // Fetch market price, only if on Strategy page
+  const { tokenDetails: baseToken } = useTokenDetails(
+    isStrategy ? baseTokenAddress : undefined
+  );
+  const { tokenDetails: quoteToken } = useTokenDetails(
+    isStrategy ? quoteTokenAddress : undefined
+  );
+  const { price } = useGetPrice({ baseToken, quoteToken });
+  const marketPrice = price?.toString() || "N/A";
+
+  // Calculate brackets, only if on Strategy page (marketPrice)
+  const { baseTokenBrackets, quoteTokenBrackets } = calculateBrackets({
+    startPrice: marketPrice,
+    totalBrackets: totalBrackets?.toString() || "",
+    lowestPrice,
+    highestPrice,
+  });
+
+  return (
+    <BracketsViewView
+      {...props}
+      startPrice={isStrategy ? marketPrice : startPrice}
+      leftBrackets={isStrategy ? baseTokenBrackets : leftBrackets}
+      rightBrackets={isStrategy ? quoteTokenBrackets : rightBrackets}
+    />
+  );
 });
