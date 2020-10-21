6d093307efce640a70854cbfb9890e8eb1b41699
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-10-21 15:05:26 -0700

    Added StrategyTab to Active/Details component

--- src/components/pages/strategies/Active/Details.tsx
+++ src/components/pages/strategies/Active/Details.tsx
@@ -1,56 +1,71 @@
 import React, { memo, useCallback, useState } from "react";
+import styled from "styled-components";
+import { Box, ButtonGroup } from "@material-ui/core";
 
 import Strategy from "logic/strategy";
-import { Box, ButtonGroup } from "@material-ui/core";
 import { Button } from "components/basic/inputs/Button";
 
-import { Props as SafeComponentButtonProps } from "@gnosis.pm/safe-react-components/dist/inputs/Button";
+import { StrategyTab } from "./StrategyTab";
 
-// I hate this
-type OldPropsWithoutColor = Omit<SafeComponentButtonProps, "color">;
-type SafeButtonProps = OldPropsWithoutColor & { color: string };
+const TabContents = styled.div`
+  padding: 35px 0;
+`;
 
 export interface Props {
   strategy: Strategy;
 }
 
-export const Details = memo(function Details({ strategy }: Props): JSX.Element {
-  const [activeDetailScreen, setActiveDetailScreen] = useState(null);
+type TabNames = "strategy" | "trades" | "params";
 
-  const makeTabChangeHandler = useCallback((tabName): (() => void) => {
-    return () => {
-      setActiveDetailScreen(tabName);
-    };
-  }, []);
+export const Details = memo(function Details({ strategy }: Props): JSX.Element {
+  const [activeDetailScreen, setActiveDetailScreen] = useState<TabNames>(
+    "strategy"
+  );
+
+  const makeTabChangeHandler = useCallback(
+    (tabName: TabNames): (() => void) => {
+      return () => {
+        setActiveDetailScreen(tabName);
+      };
+    },
+    []
+  );
 
   return (
-    <Box>
-      <ButtonGroup>
-        <Button
-          size="md"
-          variant="contained"
-          onClick={makeTabChangeHandler("strategy")}
-          color={activeDetailScreen === "strategy" ? "primary" : "white"}
-        >
-          Strategy
-        </Button>
-        <Button
-          size="md"
-          variant="contained"
-          onClick={makeTabChangeHandler("trades")}
-          color={activeDetailScreen === "trades" ? "primary" : "white"}
-        >
-          Trades
-        </Button>
-        <Button
-          size="md"
-          variant="contained"
-          onClick={makeTabChangeHandler("params")}
-          color={activeDetailScreen === "params" ? "primary" : "white"}
-        >
-          Deployed Params.
-        </Button>
-      </ButtonGroup>
-    </Box>
+    <>
+      <Box>
+        <ButtonGroup>
+          <Button
+            size="md"
+            variant="contained"
+            onClick={makeTabChangeHandler("strategy")}
+            color={activeDetailScreen === "strategy" ? "primary" : "white"}
+          >
+            Strategy
+          </Button>
+          <Button
+            size="md"
+            variant="contained"
+            onClick={makeTabChangeHandler("trades")}
+            color={activeDetailScreen === "trades" ? "primary" : "white"}
+          >
+            Trades
+          </Button>
+          <Button
+            size="md"
+            variant="contained"
+            onClick={makeTabChangeHandler("params")}
+            color={activeDetailScreen === "params" ? "primary" : "white"}
+          >
+            Deployed Params.
+          </Button>
+        </ButtonGroup>
+      </Box>
+      <TabContents>
+        {activeDetailScreen === "strategy" && (
+          <StrategyTab strategy={strategy} />
+        )}
+      </TabContents>
+    </>
   );
 });
