9b849a68fa7d4781c2dbbb492dc644f360c3981c
Andre <andre@gnosis.pm>
2020-10-27 15:05:02 +0100

    withdraw button logic still wonky, added deployed params (#132)

--- src/components/pages/strategies/Active/Details.tsx
+++ src/components/pages/strategies/Active/Details.tsx
@@ -1,12 +1,19 @@
-import React, { memo, useCallback, useState } from "react";
+import React, { memo, useCallback, useState, useContext } from "react";
 import styled from "styled-components";
-import { Box, ButtonGroup } from "@material-ui/core";
+import { ButtonGroup } from "@material-ui/core";
 
 import Strategy from "logic/strategy";
 import { Button } from "components/basic/inputs/Button";
 
 import { StrategyTab } from "./StrategyTab";
 import { WithdrawButton } from "./WithdrawButton";
+import { DeployedParamsTab } from "./DeployedParamsTab";
+
+import useInterval from "@use-it/interval";
+import {
+  ContractInteractionContext,
+  ContractInteractionContextProps,
+} from "components/context/ContractInteractionProvider";
 
 const Header = styled.div`
   display: flex;
@@ -23,10 +30,33 @@ export interface Props {
 
 type TabNames = "strategy" | "trades" | "params";
 
-export const Details = memo(function Details({ strategy }: Props): JSX.Element {
+let isFetching = false;
+export const Details = function Details({ strategy }: Props): JSX.Element {
   const [activeDetailScreen, setActiveDetailScreen] = useState<TabNames>(
     "strategy"
   );
+  const [hotStrategy, setHotStrategy] = useState(strategy);
+
+  const ctx = useContext(ContractInteractionContext);
+
+  const updateHotStrategy = useCallback(
+    async (ctx: ContractInteractionContextProps): Promise<void> => {
+      if (isFetching) return;
+      isFetching = true;
+      console.log("updating a strategy");
+      // im so sorry
+      await strategy.fetchAllPossibleInfo(ctx);
+      console.log(strategy);
+      setHotStrategy(strategy);
+      isFetching = false;
+    },
+    [strategy, setHotStrategy]
+  );
+
+  useInterval(() => {
+    console.log("updating strategy selected in detail screen");
+    updateHotStrategy(ctx);
+  }, 10000);
 
   const makeTabChangeHandler = useCallback(
     (tabName: TabNames): (() => void) => {
@@ -36,6 +66,7 @@ export const Details = memo(function Details({ strategy }: Props): JSX.Element {
     },
     []
   );
+  console.log(hotStrategy);
 
   return (
     <>
@@ -66,13 +97,16 @@ export const Details = memo(function Details({ strategy }: Props): JSX.Element {
             Deployed Params.
           </Button>
         </ButtonGroup>
-        <WithdrawButton strategy={strategy} />
+        <WithdrawButton strategy={hotStrategy} />
       </Header>
       <TabContents>
         {activeDetailScreen === "strategy" && (
-          <StrategyTab strategy={strategy} />
+          <StrategyTab strategy={hotStrategy} />
+        )}
+        {activeDetailScreen === "params" && (
+          <DeployedParamsTab strategy={hotStrategy} />
         )}
       </TabContents>
     </>
   );
-});
+};
