25d1ef48253d9ce8ee688a63bd482638e8310beb
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-11-04 15:08:39 -0800

    Added api for dexPriceEstimator, heavily inspited by dex-react

---
+++ src/api/dexPriceEstimator.ts
@@ -0,0 +1,68 @@
+import Decimal from "decimal.js";
+
+import { assert } from "@gnosis.pm/dex-js";
+
+import { DEX_PRICE_ESTIMATOR_URLS } from "utils/constants";
+
+type GetBestAskParams = {
+  networkId: number;
+  baseTokenId: number;
+  quoteTokenId: number;
+};
+
+export async function getBestAsk(
+  params: GetBestAskParams
+): Promise<Decimal | null> {
+  const { networkId, baseTokenId, quoteTokenId } = params;
+
+  // Query format: markets/7-1/estimated-best-ask-price?unit=baseunits&roundingBuffer=enabled
+  const queryString = `markets/${baseTokenId}-${quoteTokenId}/estimated-best-ask-price`;
+
+  try {
+    const response = await query<number>(networkId, queryString);
+
+    if (response === null) {
+      return null;
+    }
+
+    return new Decimal(response);
+  } catch (e) {
+    console.error(e);
+    throw new Error(
+      `Failed to query best ask for baseToken id ${baseTokenId} quoteToken id ${quoteTokenId}: ${e.message}`
+    );
+  }
+}
+
+async function query<T>(
+  networkId: number,
+  queryString: string
+): Promise<T | null> {
+  const baseUrl = getBaseUrl(networkId);
+
+  const url = baseUrl + queryString;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Request failed: [${response.status}] ${response.body}`);
+  }
+
+  const body = await response.text();
+
+  if (!body) {
+    return null;
+  }
+
+  return JSON.parse(body);
+}
+
+function getBaseUrl(networkId: number): string {
+  const baseUrl = DEX_PRICE_ESTIMATOR_URLS[networkId];
+  assert(
+    baseUrl,
+    `Dex-price-estimator not available for network id ${networkId}`
+  );
+
+  return baseUrl + "/api/v1/";
+}
