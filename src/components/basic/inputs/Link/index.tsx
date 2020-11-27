9a8ecda55152e5d96e8152a7de7afbc0f619e862
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-11-27 09:30:00 -0800

    Optionally display <a> as <button>

--- src/components/basic/inputs/Link/index.tsx
+++ src/components/basic/inputs/Link/index.tsx
@@ -1,6 +1,8 @@
 import React, { ComponentPropsWithoutRef, memo } from "react";
 import styled from "styled-components";
 
+import { Button } from "@gnosis.pm/safe-react-components";
+
 import { Text } from "components/basic/display/Text";
 
 import { theme } from "theme";
@@ -8,17 +10,16 @@ import { theme } from "theme";
 import { Props as ButtonLinkProps } from "components/basic/inputs/ButtonLink";
 
 export type Props = Pick<ButtonLinkProps, "textSize" | "color"> &
-  ComponentPropsWithoutRef<"a">;
+  ComponentPropsWithoutRef<"a"> & { as?: "button" | "a" };
 
+// Styling copied from https://github.com/gnosis/safe-react-components/blob/development/src/inputs/ButtonLink/index.tsx
+// because we need an <a> tag but SRC only provides a <button>
 const StyledLink = styled.a<Props>`
-  // Styling copied from https://github.com/gnosis/safe-react-components/blob/development/src/inputs/ButtonLink/index.tsx
-  // because we need an <a> tag but SRC only provides a <button>
-
   background: transparent;
   border: none;
   text-decoration: underline;
   cursor: pointer;
-  color: ${({ color }: Props) => theme.colors[color as string]};
+  color: ${({ color }: Props) => theme.colors[color]};
   font-family: inherit;
 
   :focus {
@@ -31,16 +32,31 @@ const StyledLink = styled.a<Props>`
     text-transform: uppercase;
     font-weight: bold;
   }
+
+  & > button {
+    width: 100%;
+  }
 `;
 
 export const component: React.FC<Props> = (props) => {
-  const { children, ...rest } = props;
+  const { children, as, ...rest } = props;
+
+  let buttonColor: React.ComponentProps<typeof Button>["color"] = "primary";
+  if (rest.color === "error" || rest.color === "secondary") {
+    buttonColor = rest.color;
+  }
 
   return (
     <StyledLink {...rest} target="_blank" rel="noopener noreferrer">
-      <Text size={props.textSize} color={rest.color} as="span">
-        {children}
-      </Text>
+      {as === "button" ? (
+        <Button size="md" color={buttonColor}>
+          {children}
+        </Button>
+      ) : (
+        <Text size={props.textSize} color={rest.color} as="span">
+          {children}
+        </Text>
+      )}
     </StyledLink>
   );
 };
