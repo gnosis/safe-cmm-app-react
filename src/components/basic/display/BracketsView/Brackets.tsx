8f173dc57bc6cc6b9282bad16faecb554d1ac56a
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-12-08 14:39:54 -0800

    Refactored brackets css

--- src/components/basic/display/BracketsView/Brackets.tsx
+++ src/components/basic/display/BracketsView/Brackets.tsx
@@ -9,65 +9,50 @@ function buildBorder(color: string): string {
   return `2px solid ${color}`;
 }
 
-const Wrapper = styled.div<{ hasLeftBrackets?: boolean }>`
+const Wrapper = styled.div`
   height: inherit;
   display: flex;
-
-  .left:first-of-type {
-    border-left: ${buildBorder(theme.colors.borderDarkGreen)};
-  }
-
-  .left {
-    background-color: ${theme.colors.backgroundLightGreen};
-
-    border-right: ${buildBorder(theme.colors.borderDarkGreen)};
-
-    &.deploy {
-      border-right: ${buildBorder(theme.colors.borderLightGreen)};
-    }
-
-    &:not(.deploy).hover,
-    &:not(.deploy):hover {
-      background-color: ${theme.colors.borderLightGreen};
-    }
-  }
-
-  .left:last-of-type {
-    border-right: ${buildBorder(theme.colors.borderDarkGreen)};
-  }
-
-  ${({ hasLeftBrackets }) =>
-    hasLeftBrackets
-      ? ""
-      : `
-  .right:first-of-type {
-    border-left: ${buildBorder(theme.colors.borderDarkPurple)};
-  }`}
-
-  .right {
-    background-color: ${theme.colors.backgroundLightPurple};
-
-    border-right: ${buildBorder(theme.colors.borderDarkPurple)};
-
-    &.deploy {
-      border-right: ${buildBorder(theme.colors.borderLightPurple)};
-    }
-
-    &:not(.deploy).hover,
-    &:not(.deploy):hover {
-      background-color: ${theme.colors.borderLightPurple};
-    }
-  }
-
-  .right:last-of-type {
-    border-right: ${buildBorder(theme.colors.borderDarkPurple)};
-  }
 `;
 
 const Bracket = styled.div<{
+  type: "left" | "right";
   width: number;
+  hasLeftBrackets?: boolean;
 }>`
   width: ${({ width }) => width}%;
+
+  ${({ type, hasLeftBrackets }) => {
+    const colorSuffix = type === "left" ? "Green" : "Purple";
+    const firstOfType =
+      (type === "left" || !hasLeftBrackets) &&
+      `&:first-of-type {
+            border-left: ${buildBorder(
+              theme.colors[`borderDark${colorSuffix}`]
+            )};
+          }`;
+
+    return `
+      ${firstOfType || ""}
+
+      background-color: ${theme.colors[`backgroundLight${colorSuffix}`]};
+  
+      border-right: ${buildBorder(theme.colors[`borderDark${colorSuffix}`])};
+  
+      &.deploy {
+        border-right: ${buildBorder(theme.colors[`borderLight${colorSuffix}`])};
+      }
+  
+      &:not(.deploy).hover,
+      &:not(.deploy):hover {
+        background-color: ${theme.colors[`borderLight${colorSuffix}`]};
+      }
+    }
+  
+    &:last-of-type {
+      border-right: ${buildBorder(theme.colors[`borderDark${colorSuffix}`])};
+    }
+    `;
+  }}
 `;
 
 export const Brackets = memo(function Brackets(): JSX.Element {
@@ -81,21 +66,22 @@ export const Brackets = memo(function Brackets(): JSX.Element {
     (id: number): string => {
       const classes = [];
 
-      classes.push(id < leftBrackets ? "left" : "right");
       hoverId === id && classes.push("hover");
       type === "deploy" && classes.push("deploy");
 
       return classes.join(" ");
     },
-    [hoverId, leftBrackets, type]
+    [hoverId, type]
   );
 
   return (
-    <Wrapper hasLeftBrackets={leftBrackets > 0}>
+    <Wrapper>
       {bracketsSizes.map((size, id) => (
         <Bracket
           key={id}
           width={size}
+          hasLeftBrackets={leftBrackets > 0}
+          type={id < leftBrackets ? "left" : "right"}
           className={buildClasses(id)}
           onMouseEnter={() => onHover && onHover(id)}
           onMouseLeave={onMouseLeave}
