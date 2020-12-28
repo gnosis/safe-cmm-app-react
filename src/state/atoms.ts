9424f73f3705802f92be92410fa391b582d89e6f
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-12-28 13:32:50 -0800

    New atoms/selectors for trades

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -1,12 +1,14 @@
-import { atom } from "recoil";
+import { atom, atomFamily, selectorFamily } from "recoil";
 import BN from "bn.js";
 
 import {
   LoadingState,
   StrategyState,
   TokenDetails,
+  TradesState,
   WithdrawState,
 } from "types";
+import { Trade } from "api/web3/trades";
 
 export const tokenBalancesState = atom<Record<string, BN>>({
   key: "tokenBalances",
@@ -32,4 +34,39 @@ export const strategiesLoadingState = atom<LoadingState>({
 export const tokenDetailsState = atom<Record<string, TokenDetails>>({
   key: "tokenDetails",
   default: {},
+});
+
+export const strategyTradesStateFamily = atomFamily<TradesState, string>({
+  key: "tradesState",
+  default: { trades: [], lastCheckedBlock: 0 }, // TODO: try local storage first
+  effects_UNSTABLE: (txHash: string) => [
+    ({ onSet }): void =>
+      onSet((state) =>
+        console.debug(`new trades state saved ${txHash}`, state)
+      ),
+  ],
+});
+
+export const tradesSelector = selectorFamily<Trade[], string>({
+  key: "trades",
+  get: (txHash: string) => ({ get }) =>
+    get(strategyTradesStateFamily(txHash)).trades,
+  set: (txHash: string) => ({ get, set }, trades): void => {
+    const tradesStateAtom = strategyTradesStateFamily(txHash);
+    const tradesState = get(tradesStateAtom);
+
+    set(tradesStateAtom, { ...tradesState, trades });
+  },
+});
+
+export const lastCheckedBlockSelector = selectorFamily<number, string>({
+  key: "lastCheckedBlock",
+  get: (txHash: string) => ({ get }) =>
+    get(strategyTradesStateFamily(txHash)).lastCheckedBlock,
+  set: (txHash: string) => ({ get, set }, lastCheckedBlock): void => {
+    const tradesStateAtom = strategyTradesStateFamily(txHash);
+    const tradesState = get(tradesStateAtom);
+
+    set(tradesStateAtom, { ...tradesState, lastCheckedBlock });
+  },
 });
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,8 @@ import Decimal from "decimal.js";
 
 import { Bracket, PriceRange } from "logic/IStrategy";
 
+import { Trade } from "api/web3/trades";
+
 export interface TokenDetails {
   address: string;
   decimals: number;
@@ -59,6 +61,11 @@ export interface StrategyState {
   hasFetchedBalance: boolean;
   hasFetchedStatus: boolean;
   hasFetchedFunding: boolean;
+}
+
+export interface TradesState {
+  trades: Trade[];
+  lastCheckedBlock: number;
 }
 
 export type Unpromise<T> = T extends Promise<infer U> ? U : T;
